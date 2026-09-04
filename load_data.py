49e8205158e626cc3bf121d3c4be664889866f32
ChazCisneros20 <chazcisneros@gmail.com>
2026-09-03 19:33:47 -0700

    Fixed initialization

--- load_data.py
+++ load_data.py
@@ -5,7 +5,7 @@ df = pd.read_csv('cell-count.csv')
 
 
 
-with sqlite3.connection('databaseSchema.db') as conn:
+with sqlite3.connect('databaseSchema.db') as conn:
     curr = conn.cursor()
 
     #=== CREATE TABLES ======
@@ -26,8 +26,8 @@ with sqlite3.connection('databaseSchema.db') as conn:
             FOREIGN KEY (subject) REFERENCES Subject(subject)
         );
         CREATE TABLE IF NOT EXISTS Sample(
-            sample TEXT PRIMARY KEY AUTOINCREMENT, 
-            subject INTEGER NOT NULL, 
+            sample TEXT PRIMARY KEY, 
+            subject TEXT NOT NULL, 
             sample_type TEXT NOT NULL, 
             time_from_treatment_start TEXT NOT NULL CHECK time_from_treatment_start >= 0,
             FOREIGN KEY (subject) REFERENCES Subject(subject)
@@ -37,30 +37,56 @@ with sqlite3.connection('databaseSchema.db') as conn:
             population TEXT NOT NULL, 
             percentage DECIMAL NOT NULL, 
             count INTEGER NOT NULL, 
-            total_count INTEGER NOT NULL
+            total_count INTEGER NOT NULL,
             PRIMARY KEY (sample, population),
             FOREIGN KEY (sample) REFERENCES Sample(sample)
         );
         """
 
     )
-    for useless_index, row in df[::3].iterrows():
+    i=0
+    for useless_index, row in df.iterrows():
+        if i % 3 == 0:
+            curr.execute(
+                f"""
+                INSERT INTO Subject(subject, condition, age, sex, project) VALUES
+                (
+                    "{row['subject']}", "{row['condition']}", {row['age']}, "{row['sex']}", "{row['project']}"  
+                );
+                """
+            )
+            curr.execute(
+                f"""
+                INSERT INTO Treatment(subject, treatment, response) VALUES
+                (
+                    "{row['subject']}", "{row['treatment']}", "{row['response']}"
+                );
+                """
+            )
         curr.execute(
             f""""
-            INSERT INTO Subject(subject, condition, age, sex, project) VALUES
+            INSERT INTO Sample(sample, subject, sample_type, time_from_treatment_start) VALUES
             (
-                {row['subject']}, {row['condition']}, {row['age']}, {row['sex']}, {row['project']}  
-            );
-            INSERT INTO Treatment(subject, treatment, response) VALUES
-            (
-                {row['subject']}, {row['treatment']}, {row['response']}
+                "{row['sample']}", "{row['subject']}", "{row['sample_type']}", {row['time_from_treatment_start']}
             );
             """
         )
-    for useless_index, row in df.iterrows():
-        curr.execute(
-            
-        )
+        i+=1
+    for uslsindx, row in df.iterrows():
+        populations = ['b_cell', 'cd8_t_cell', 'cd4_t_cell', 'nk_cell', 'monocyte']
+        totalCount = 0
+        for i in range(5):
+            totalCount += row[populations[i]]
+        for i in range(5):
+            #this should create 5 rows based on the 3 sample records in the original csv.
+            curr.execute(
+                f"""
+                INSERT INTO CellFrequency(sample, population, percentage, count, total_count) VALUES
+                (
+                    {row['sample']}, {populations[i]}, {row[populations[i]] / totalCount}, {row[populations[i]]}, {totalCount}
+                )
+                """
+            )
     
 
 
